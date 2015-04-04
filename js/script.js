b696172ea41e2992452057cb69cc16f1e6126a07
oaglazunova <oaglazunova@gmail.com>
2015-04-04 15:54:58 +0300

    ---

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function initialize() {
+      var mapCanvas = document.getElementById('map');	  
+      var myLatLng = new google.maps.LatLng(59.936148, 30.322917);
+      var mapOptions = {
+        center: myLatLng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true
+      }
+      var map = new google.maps.Map(mapCanvas, mapOptions); 
+	  
+	  var image = 'img/map-marker.png';
+      var beachMarker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      icon: image
+  });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+	
+  var toggler = document.getElementById('toggler');
+    toggler.onclick = function (e) {
+      e.preventDefault();
+      toggler.classList.toggle('menu-toggler--close');
+    } 
